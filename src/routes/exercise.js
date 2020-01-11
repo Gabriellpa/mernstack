8c2fc15bda9669a15343227c35148b2e7c15866e
Gabriellpa <43101962+Gabriellpa@users.noreply.github.com>
2020-01-11 14:10:17 -0300

    Rotas de find e update adicionadas

--- src/routes/exercise.js
+++ src/routes/exercise.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 
 router.get('/', exerciseController.find);
 router.post('/', exerciseController.store);
+router.get('/:id', exerciseController.findById);
+router.post('/:id', exerciseController.update);
 
 module.exports = router;
